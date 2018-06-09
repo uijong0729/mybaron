7c1317e03349a843a4195928a36c70d58abf4771
EuijongLee <uijong0729@naver.com>
2018-06-09 22:16:54 +0900

    로그인 구현하다가 롤하러 감

--- src/main/java/org/interior/controller/HomeController.java
+++ src/main/java/org/interior/controller/HomeController.java
@@ -2,11 +2,13 @@ package org.interior.controller;
 import org.interior.repository.User;
 import org.interior.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -40,7 +42,15 @@ public class HomeController {
 		return "/user/list";
 	}
 	
-	
+	@ResponseBody
+	@PostMapping("/requestLogin")
+	public String requestLogin(User user) {
+		
+		//System.out.println(user);
+		
+		return "엿머거라";
+		
+	}
 	
 	
 	
--- src/main/java/org/interior/repository/User.java
+++ src/main/java/org/interior/repository/User.java
@@ -64,6 +64,12 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", comments=" + comments + ", password=" + password + "]";
+	}
+	
 	
 	
 }
