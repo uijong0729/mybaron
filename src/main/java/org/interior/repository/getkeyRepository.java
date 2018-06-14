309bc1b11d548f4989f2460f2093f79805309e4a
이의종 <krocha0729@gmail.com>
2018-06-14 19:31:19 +0900

    getkeyRepo
    commit

--- src/main/java/org/interior/repository/getkeyRepository.java
+++ src/main/java/org/interior/repository/getkeyRepository.java
@@ -2,7 +2,7 @@ package org.interior.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 								//<테이블이름, id의 자료형타입>
-public interface RewriteKeyRepository extends JpaRepository<RewriteKey, Long>{
+public interface getkeyRepository extends JpaRepository<Geykey, Long>{
 	
 	
 }
