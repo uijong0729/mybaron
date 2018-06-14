658d6ec73d3c05ea563534bb03bfdc4b9a596d1b
NaSeungHwan <nsh356@naver.com>
2018-06-14 21:28:06 +0900

    key 는 mysql 예약어로 컬럼명으로 사용 불가, api_key 로 컬럼 명 변경

--- src/main/java/org/interior/repository/Api.java
+++ src/main/java/org/interior/repository/Api.java
@@ -5,25 +5,23 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Api {
-	
+
 	@Id
 	@Column(name="Id", nullable = false, length = 10)
 	private Long Id;
-	
-	@Column(name="key", nullable = false, length = 512)
+
+	@Column(name="api_key", nullable = false, length = 512)
 	private String key;
 }
